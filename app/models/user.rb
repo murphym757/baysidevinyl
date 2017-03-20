0a242fba8722ae2bd16bd68b70302778ee97d330
murphym757 <murphym757@yahoo.com>
2017-03-20 14:41:37 -0400

    Added the ability to have a username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,24 +8,25 @@ class User < ApplicationRecord
 
   attr_accessor :login
 
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+end
 
-  validates :username,
-    :presence => true,
-    :uniqueness => {
-      :case_sensitive => false
-    }
+validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
-  def self.find_first_by_auth_conditions(warden_conditions)
-    conditions = warden_conditions.dup
-    if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  }
+
+def self.find_first_by_auth_conditions(warden_conditions)
+  conditions = warden_conditions.dup
+  if login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  else
+    if conditions[:username].nil?
+      where(conditions).first
     else
-      if conditions[:username].nil?
-        where(conditions).first
-      else
-        where(username: conditions[:username]).first
-      end
+      where(username: conditions[:username]).first
     end
   end
 end
