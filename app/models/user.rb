f40cf363143ceadf5d67d45f6f3eb3cbf85ec851
murphym757 <murphym757@yahoo.com>
2017-03-31 15:31:12 -0400

    Changed the overall appearance of the homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
   has_many :vinyls
 
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 
   attr_accessor :login
@@ -17,6 +15,35 @@ validate :validate_username
   validates :username,
     presence: true,
     uniqueness: { case_sensitive: false }
+
+    def login=(login)
+      @login = login
+    end
+
+    def login
+      @login || self.username || self.email
+    end
+
+
+    def validate_username
+      if User.where(email: username).exists?
+        errors.add(:username, :invalid)
+      end
+    end
+
+
+    def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+      else
+        if conditions[:username].nil?
+          where(conditions).first
+        else
+          where(username: conditions[:username]).first
+        end
+      end
+    end
 end
 
 
--- db/migrate/20170321042505_add_avatars_to_users.rb
+++ db/migrate/20170321042505_add_avatars_to_users.rb
@@ -1,11 +1,9 @@
 class AddAvatarsToUsers < ActiveRecord::Migration[5.0]
   def self.up
-    change_table :users do |t|
-      t.attachment :avatar
+    add_attachment :users
     end
-  end
 
   def self.down
-    drop_attached_file :users, :avatar
+    remove_attachment :users
   end
 end
