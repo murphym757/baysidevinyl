f8a98ae2c387dbc8235d67c7a06397ad1b306719
murphym757 <murphym757@yahoo.com>
2017-04-15 18:03:04 -0400

    modified track ruby file for images

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,7 +3,7 @@ class Track < ApplicationRecord
   belongs_to :user
 
     has_attached_file :image, styles: { medium: "400x400#" }
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+    validates_attachment :image, content_type:  { content_type: [/\Aimage\/.*\z/] }
 
     has_attached_file :mp3
     validates_attachment :mp3, content_type: { content_type: ["audio/mpeg", "audio/mp3"] }, file_name: { matches: [/mp3\z/] }
